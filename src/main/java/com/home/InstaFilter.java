4477549304b9cf87641377621d0653e2c458724f
Nihrom77 <Snoll@yandex.ru>
2018-04-18 22:58:55 +0300

    Убрал пользователей в json

--- src/main/java/com/home/InstaFilter.java
+++ src/main/java/com/home/InstaFilter.java
@@ -1,21 +1,42 @@
 package com.home;
 
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.ArrayList;
-import java.util.Arrays;
+import java.util.Iterator;
 import java.util.List;
 
 public class InstaFilter {
-    String[] myUsers= {"gusiona_lapchataya","_ilya_.nik","sveta.zim.sz","senkovskiy","iriyasan","helly___belly",
-    "khan_shu","nookerrr","zhenya.voynov","anastasya.dyachenko","ilya_a_orlov","kostiko8","artyom.zemskov",
-    "natalia.malcina","ilya2710","vanadium50","shib_vit","anastasilz","alwayshelix","gusiona_lapchataya",
-    "dyakov.vladimir","irashmatova","vladimir_ignatoff","_artartem"};
-    List<String> users;
 
-    public InstaFilter(){
-        users = Arrays.asList(myUsers);
+    private List<String> users = new ArrayList<>();
+
+    public InstaFilter() {
+        JSONParser parser = new JSONParser();
+        try {
+            Object obj = parser.parse(new FileReader("target/classes/MyFavoriteUsers.json"));
+            JSONObject jsonObject = (JSONObject) obj;
+
+            JSONArray users1 = (JSONArray) jsonObject.get("users");
+
+
+            Iterator<String> iterator = users1.iterator();
+            while (iterator.hasNext()) {
+                users.add(iterator.next());
+            }
+
+
+        } catch (IOException | ParseException e) {
+            e.printStackTrace();
+        }
+
     }
 
-    public boolean isUserInFilter(String user){
+    public boolean isUserInFilter(String user) {
         return users.contains(user);
     }
 }
